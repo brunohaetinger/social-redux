5f953c2740fc34dcd3f5081f4ea2c81bc309ba3a
brunohaetinger <brunohaetinger@gmail.com>
2021-03-13 00:11:26 -0300

    Do async requests to fake backend

---
+++ src/features/posts/PostExcerpt.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { PostAuthor } from './PostAuthor';
+import { ReactionButtons } from './ReactionButtons';
+import { TimeAgo } from './TimeAgo';
+
+const PostExcerpt = ({post}) => {
+  return (
+    <article className="post-excerpt" key={post.id}>
+    <h3>{post.title}</h3>
+    <div>
+      <PostAuthor userId={post.user} />
+      <TimeAgo timestamp={post.date} />
+    </div>
+    <p className="post-content">{post.content.substring(0, 100)}</p>
+    <Link to={`/posts/${post.id}`} className="button muted-button">
+      View Post
+    </Link>
+    <ReactionButtons post={post}></ReactionButtons>
+  </article>
+  )
+}
+
+export default PostExcerpt;
